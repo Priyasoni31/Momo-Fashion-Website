efe6c5dca3e3b7a429ee51144b098dc4e42001b4
Priya Soni <priyaa.soni31@gmail.com>
2022-06-22 11:55:35 +0530

    first commit

---
+++ file.js
@@ -0,0 +1,39 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+
+//initialising a variable name data
+  
+var data = 0;
+  
+//printing default value of data that is 0 in h2 tag
+document.getElementById("counting").innerText = data;
+  
+//creation of increment function
+function increment() {
+    data = data + 1;
+    document.getElementById("counting").innerText = data;
+}
+//creation of decrement function
+function decrement() {
+    data = data - 1;
+    document.getElementById("counting").innerText = data;
+}
